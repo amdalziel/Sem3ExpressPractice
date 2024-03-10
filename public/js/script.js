e0f3715b1a77efaed715e3e9411688335afddeb4
Amy Dalziel <amy.dalziel@keyin.com>
2024-03-09 21:37:16 -0330

    pie chart working

--- public/js/script.js
+++ public/js/script.js
@@ -1,68 +1,39 @@
-// Battle of Beer Chart 
 
-const DATA_COUNT = 5;
-const NUMBER_CFG = {count: DATA_COUNT, min: 0, max: 100};
+const background_colors = {
+    brightYellow: '#F2A007',
+    brightOrange: '#F28F16',
+    darkOrange: '#BF5B04',
+    brown: '#592202', 
+    beige: '#D9B07E',
+}; 
 
-const CHART_COLORS = {
-    red: 'rgb(255, 99, 132)',
-    orange: 'rgb(255, 159, 64)',
-    yellow: 'rgb(255, 205, 86)',
-    green: 'rgb(75, 192, 192)',
-    blue: 'rgb(54, 162, 235)',
-    purple: 'rgb(153, 102, 255)',
-    grey: 'rgb(201, 203, 207)'
-  };
+(async function() {
+  const data = [
+    { product: 'Happy Ale', count: 10 },
+    { product: 'Smooth Sailing', count: 20 },
+    { product: 'Bitter End', count: 50 },
+    { product: 'Hoppy Heart', count: 5 },
+    { product: 'Golden Times', count: 15 },
+  ];
 
-
-  function numbers(config) {
-    var cfg = config || {};
-    var min = 0; 
-    var max = 100; 
-    var from = []; 
-    var count = 8; 
-    var decimals = 8; 
-    var continuity = 1; 
-    var dfactor = Math.pow(10, decimals) || 0;
-    var data = [];
-    var i, value;
-  
-    for (i = 0; i < count; ++i) {
-      value = (from[i] || 0) + this.rand(min, max);
-      if (this.rand() <= continuity) {
-        data.push(Math.round(dfactor * value) / dfactor);
-      } else {
-        data.push(null);
-      }
-    }
-  
-    return data;
-  }
-
-const data = {
-  labels: ['Red', 'Orange', 'Yellow', 'Green', 'Blue'],
-  datasets: [
+  new Chart(
+    document.getElementById('beerGraph'),
     {
-      label: 'Dataset 1',
-      data: numbers(NUMBER_CFG),
-      backgroundColor: Object.values(CHART_COLORS),
-    }
-  ]
-};
-
-
-const beerGraph = new Chart("beerGraph", {
-    type: 'pie',
-    data: data,
-    options: {
-      responsive: true,
-      plugins: {
-        legend: {
-          position: 'top',
-        },
-        title: {
-          display: true,
-          text: 'Chart.js Pie Chart'
-        }
+      type: 'pie',
+      data: {
+        labels: data.map(row => row.product),
+        datasets: [
+          {
+            label: 'Beers!',
+            data: data.map(row => row.count),
+            backgroundColor: Object.values(background_colors),
+          }
+        ]
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Pie Chart'
       }
-    },
-  }); 
+    }
+  );
+})();
